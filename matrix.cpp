0456259d5219be28374fd4d00b9bb0969f214b52
[Jade Holmes] <holmes2003dd@gmail.com>
2024-05-25 01:52:21 -0400

    added reduced echelon form function, made small adjustments to output format

--- matrix.cpp
+++ matrix.cpp
@@ -1,3 +1,5 @@
+// Converts a matrix into echelon form using Gaussian Elimination
+
 #include <iostream>
 #include <vector>
 #include <iomanip>
@@ -8,36 +10,39 @@
 
 #define FLOAT_MIN -80000000.0
 
-// using namespace std; (best practice to ommit this)
-
-// needs fixing :(
-void printMatrix(std::vector<std::vector<float>> matrix){
+void printMatrix(std::vector<std::vector<float>> &matrix){
     int rows = matrix.size();
     int cols = matrix[0].size(); 
 
-    std::cout << std::setw((6*cols) - (cols)) << std::left << " _";
-    std::cout << std::setw(5) << std::right << "_";
+    std::cout << std::setw((6*cols) + 1) << std::left << " _";
+    std::cout << std::setw((5*cols) - 2) << std::right << "_";
     std::cout << "\n";
 
     int i, j;
 
     for (i = 0; i < rows; i++){
         if (i == rows-1){
-            std::cout << std::setw(5) << std::left << "|_";
+            std::cout << std::setw(6) << std::left << "|_";
 
-            for (j = 0; j < cols; j++)
-                std::cout << matrix[i][j] << std::setprecision(2) << std::setw(4) << std::right << " ";
+            for (j = 0; j < cols; j++){
+                if (j == cols-1){
+                    std::cout << std::fixed << std::setprecision(2) << std::setw(7) << std::left << matrix[i][j]; 
+                }
+                else {
+                    std::cout << std::fixed << std::setprecision(2) << std::setw(9) << std::left << matrix[i][j];
+                }
+            }
 
             std::cout << "_|";
         }
 
         else {
-            std::cout << std::setw(5) << std::left << "|";
+            std::cout << "|";
 
             for (j = 0; j < cols; j++)
-                std::cout << std::setw(5) << std::setprecision(2) << std::left << matrix[i][j];
+                std::cout << std::fixed << std::setprecision(2) << std::setw(9)  << std::right << matrix[i][j];
 
-            std::cout << "|"; 
+            std::cout << std::setw(5) << "|"; 
             std::cout << "\n";
         }
     }
@@ -45,7 +50,8 @@ void printMatrix(std::vector<std::vector<float>> matrix){
     std::cout << "\n";
 }
 
-void echelonForm(std::vector<std::vector<float>> matrix){
+void echelonForm(std::vector<std::vector<float>> &matrix){
+    std::cout << "\n Matrix in Echelon Form:\n";
     int row, col;
     row = matrix.size();
     col = matrix[0].size();
@@ -63,26 +69,72 @@ void echelonForm(std::vector<std::vector<float>> matrix){
         }
 
         // If no pivot is found in this row, move to the end of the matrix
-        if (pivot == INT_MIN){
+        if (pivot == FLOAT_MIN){
             matrix.push_back(matrix[i]);
             matrix.erase(matrix.begin() + i);
         }
 
         // Make all columns under the pivot = 0
         else {
-            // Keep track of the current row
+            // Keep track of the current row and the row of the pivot
+            int loc = i;
             int cur = i+1;
 
-            if (cur == row-1)
-                break;
-            // x is the value to use in the row replacement operation
-            float x = (matrix[cur][pivot] / matrix[cur-1][pivot]);
-
             while (cur < row){
+                // x is the value to use in the row replacement operation
+                float x = (matrix[cur][pivot] / matrix[loc][pivot]);
+
                 for(int i = 0; i < col; i++)
-                    matrix[cur][i] = matrix[cur][i] - (x * matrix[cur-1][i]);
+                    matrix[cur][i] = matrix[cur][i] - (x * matrix[loc][i]);
 
                 cur++;
+            }
+        }
+    }
+
+    printMatrix(matrix);
+}
+
+void reducedForm(std::vector<std::vector<float>> &matrix){
+    std::cout << "\n Matrix in Reduced Echelon Form:\n";
+    int row, col;
+    row = matrix.size();
+    col = matrix[0].size();
+
+    int i, j;
+    for (int i = row-1; i >= 0; i--){
+        float pivot = FLOAT_MIN;
+
+        for (int j = 0; j < col-2; j++){
+            // Find pivot
+            if (matrix[i][j] != 0){
+                pivot = j;
+                break;
+            }
+        }
+
+        // If no pivot is found in this row, move on 
+        if (pivot == FLOAT_MIN){
+            continue;
+        }
+
+        else if (i != 0) {
+            // Keep track of the current row and the row of the pivot
+            int loc = i;
+            int cur = i-1;
+            
+            // d is the scalar used to make the pivot 1 (which is 1/pivot)
+            float d = matrix[loc][pivot];
+            float x = matrix[cur][pivot];
+
+            // Divide every value in the same row by the scalar
+            // Then, make the values above the pivot = 0
+            while (cur >= 0){
+                for(int i = 0; i < col; i++){
+                    matrix[loc][i] /= d;
+                    matrix[cur][i] = matrix[cur][i] - (x * matrix[loc][i]); 
+                }
+                cur--;
             }
         }
     }
@@ -93,7 +145,7 @@ void echelonForm(std::vector<std::vector<float>> matrix){
 int main(){
     std::cout << "Welcome to the Matrix Equation Solver!\n";
         
-    std::ifstream input("m1.txt", std::ios_base::in);
+    std::ifstream input("m34.txt", std::ios_base::in);
     if (!input.is_open()){
         std::cout << "Error opening file.";
         return 1;
@@ -120,8 +172,10 @@ int main(){
         equationSet.push_back(equation);
     }
 
+    std::cout << "\nOriginal Matrix:\n";
     printMatrix(equationSet);
     echelonForm(equationSet);
+    reducedForm(equationSet);
 
 
     return 0;
